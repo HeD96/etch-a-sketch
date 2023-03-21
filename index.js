8fdd8732e4f146d4430825d224744efa2f1c981b
imhed <hedplanet96@gmail.com>
2023-03-21 22:45:21 +0300

    continue adding nonsense 2

---
+++ index.js
@@ -0,0 +1,16 @@
+const docBody = document.body;
+const divContainer = document.createElement("div");
+const div = document.createElement("div");
+
+divContainer.classList.add("container");
+div.classList.add("square");
+
+docBody.appendChild(divContainer);
+
+for (let i = 1; i <= 16; i++) {
+    for (let j = 1; j <= 16; j++) {
+        divContainer.appendChild(div.cloneNode(true));
+    }
+}
+
+
