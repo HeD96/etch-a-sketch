e5921419a029ee253ad12f0dfdcfd17e958cbe82
imhed <hedplanet96@gmail.com>
2023-03-23 22:24:06 +0300

    added hovering state

--- index.js
+++ index.js
@@ -5,12 +5,33 @@ const div = document.createElement("div");
 divContainer.classList.add("container");
 div.classList.add("square");
 
-docBody.appendChild(divContainer);
+//making a board
+
+const btnBoard = document.querySelector("button");
+
+btnBoard.addEventListener('click', function() {
+    let numSquares = prompt("Enter number of squares");
+    console.log(numSquares);
+});
 
 for (let i = 1; i <= 16; i++) {
     for (let j = 1; j <= 16; j++) {
         divContainer.appendChild(div.cloneNode(true));
     }
 }
+
+docBody.appendChild(divContainer);
+
+//hovering
+
+const squares = document.querySelectorAll('.square');
+
+squares.forEach(function(square) {
+    square.addEventListener('mouseover', function() {
+        this.classList.add('colored');
+    });
+});
+
+
 
 
